472ce5128683482d1c76a8abb73cac2c37c6ad03
mraye <mraye@email.com>
2018-09-08 11:48:47 +0800

    add index页面

---
+++ src/main/java/com/github/vspro/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.github.vspro.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/accessDeny")
+    public String accessDeny(){
+        return "accessDeny";
+    }
+
+    @RequestMapping("/confirm_access")
+    public String confirmAccess(){
+        return "access_confirmation";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+}
